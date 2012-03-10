ce4f03ec341a79c428062686e9272ebcfe98e2f1
Dmitry Brazhnikov <dima@ecliptic.(none)>
2012-03-10 15:13:53 +0400

    task1 is ready to be examinated

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,28 +1,86 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
+#include <assert.h>
 
 using std::cout;
+using std::vector;
+using std::endl;
+
+// @overrading vectors operators 
+std::vector<int> operator + (const std::vector<int> &v1, const int &n){
+    vector<int> result(v1.size());
+    for(int i = 0; i < v1.size(); i++){
+        result[i] = v1[i] + n;
+    }
+    return result;
+}
+
+std::vector<int> operator + (const std::vector<int> &v1, const std::vector<int> &v2){
+    assert(v1.size() == v2.size());
+    
+    vector<int> result(v1.size());
+    for(int i = 0; i < v1.size(); i++){
+        result[i] = v1[i] + v2[i];
+    }
+    return result;
+}
+
+std::vector<int> operator % (const std::vector<int> &v1, const int &n){
+    vector<int> result(v1.size());
+    for(int i = 0; i < v1.size(); i++){
+        result[i] = v1[i] % n;
+    }
+    return result;
+}
+
+std::vector<int> pow(const std::vector<int> &v1, const int &n){
+    vector<int> result(v1.size());
+    for(int i = 0; i < v1.size(); i++){
+        result[i] = pow(v1[i], n);
+    }
+    return result;
+}
 
 template<class T>
-class Function{
+class CFunctor{
     public:
-        T operator () (T &x_arg){
-            return pow(x_arg, 2);
+        CFunctor(){
+            this->module = 0;
         }
+
+        CFunctor(const int &module){
+            this->module = module;
+        }
+
+        T operator () (T &arg){
+            T result = pow(arg,2);
+            if(module != 0){
+                result = result % module;
+            }
+            return result;
+        }
+
+        const int& getMod() const{
+            return module;
+        }
+    private:
+        int module;
 };
 
+
+// find size of the orbit cycle or precycle 
 template<class T>
 class PreCycle{
     public:
-        PreCycle(const Function<T> &funcToExec){
+        PreCycle(const CFunctor<T> &funcToExec){
             this->funcToExec = funcToExec;
         }
 
-        int getCycSize(const int &module, const T &initValue){
+        int getCycSize(const T &initValue){
             T value = initValue;
-            for(int i = 0; i <= module; i++){
+            for(int i = 0; i <= funcToExec.getMod(); i++){
                 value = funcToExec(value);
-                value = value % module;
             }
 
             T lookForNumber = value; 
@@ -30,7 +88,6 @@ class PreCycle{
             int cycSize = 0;
             while(isCycleFound == false){
                 value = funcToExec(value);
-                value = value % module;
                 cycSize++;
                 
                 if(lookForNumber == value){
@@ -40,21 +97,19 @@ class PreCycle{
             return cycSize;
         }
             
-        int getPreCycSize(const int &module, const T &initValue){
-            int cycSize = getCycSize(module, initValue);
+        int getPreCycSize(const T &initValue){
+            int cycSize = getCycSize(initValue);
             T value = initValue;
             T result;
             int preCycSize = 0;
             while(value != result){ 
                 for(int i = 0; i < preCycSize; i++){
                     value = funcToExec(value);
-                    value = value % module;
                 }
                                     
                 result = value;
                 for(int i = 0; i < cycSize; i++){
                     result = funcToExec(result);
-                    result = result % module;
                 }
                 
                 if(value != result) {
@@ -64,14 +119,23 @@ class PreCycle{
             return preCycSize;
         }
     private:
-        Function<T> funcToExec;
+        CFunctor<T> funcToExec;
 };
 
 int main(){
-    Function<int> func;
+    CFunctor<int> func(5);
     PreCycle<int> cyc(func);
-    cout << cyc.getPreCycSize(5, 2);
+    cout << cyc.getPreCycSize(2) << endl;
     
+    vector<int> v1;
+    v1.push_back(0); 
+    v1.push_back(1); 
+    v1.push_back(4); 
+    
+    CFunctor<vector<int> > funcV(5);
+    PreCycle<vector<int> > cycV(funcV);
+    cout << cycV.getPreCycSize(v1) << endl;
+
     return 0;
 }
 
